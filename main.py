0ac24a3065b05cce42dd3b294b15888e572d7c62
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-12 16:28:18 +0000

    Uploading project files to GitHub.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #Write your code below this line 👇
 
+def prime_checker(number):
+  is_prime = True
+  for i in range(2, number - 1):
+    if number % i == 0:
+      is_prime = False
+
+  if is_prime:
+    print("This is a prime number.")
+  else:
+    print("This is not a prime number.")
+
 
 
 
