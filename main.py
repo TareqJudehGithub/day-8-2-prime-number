8ab77802203df543250e0e4ac9867da12d227aa4
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-13 23:18:30 +0000

    Fixed a bug causing non-prime numbers to be displayed as prime in function final result.

--- main.py
+++ main.py
@@ -2,16 +2,14 @@
 
 def prime_checker(number):
   is_prime = True
-  for i in range(2, number - 1):
-    if number % i == 0:
+  for n in range(2, number):
+    if number % n == 0:
       is_prime = False
-
+    
   if is_prime:
-    print("This is a prime number.")
+    print(f"{number} is a prime number.")
   else:
-    print("This is not a prime number.")
-
-
+    print(f"{number} is not a prime number.")
 
 
 
